c6d5ee6b051233809293c0eb2673d0cb99809689
sota <git@xsota.com>
2016-05-24 22:47:18 +0900

    じゃんけんの手を表示

--- core/src/inc/syk/janken/JankenGame.java
+++ core/src/inc/syk/janken/JankenGame.java
@@ -6,28 +6,71 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 
 public class JankenGame extends ApplicationAdapter {
-  SpriteBatch batch;
-  Texture img;
+  private final int SCREEN_SIZE_WIDTH = 800;
+  private final int SCREEN_SIZE_HEIGHT = 480;
+
   OrthographicCamera camera;
+
+  SpriteBatch batch;
+
+  Texture guTexture;
+  Texture tyokiTexture;
+  Texture paTexture;
+
+  Rectangle gu;
+  Rectangle tyoki;
+  Rectangle pa;
 
 
   @Override
   public void create () {
     camera = new OrthographicCamera();
-    camera.setToOrtho(false, 800, 480); //画面サイズ
+    camera.setToOrtho(false, SCREEN_SIZE_WIDTH, SCREEN_SIZE_HEIGHT); //画面サイズ
 
     batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+
+    guTexture = new Texture("guu.png");
+    tyokiTexture = new Texture("tyoki.png");
+    paTexture = new Texture("paa.png");
+
+    gu = new Rectangle();
+    gu.x = 20;
+    gu.y = 20;
+    gu.width = guTexture.getWidth();
+    gu.height = guTexture.getHeight();
+
+    tyoki = new Rectangle();
+    tyoki.x = (SCREEN_SIZE_WIDTH / 2) - (tyokiTexture.getWidth() / 2);
+    tyoki.y = 20;
+    tyoki.width = tyokiTexture.getWidth();
+    tyoki.height = tyokiTexture.getHeight();
+
+    pa = new Rectangle();
+    pa.x = SCREEN_SIZE_WIDTH - paTexture.getWidth() - 20;
+    pa.y = 20;
+    pa.width = paTexture.getWidth();
+    pa.height = paTexture.getHeight();
+
   }
 
   @Override
   public void render () {
-    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClearColor(1, 1, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    batch.setProjectionMatrix(camera.combined);
     batch.begin();
-    batch.draw(img, 0, 0);
+
+
+    batch.draw(guTexture, gu.x, gu.y);
+    batch.draw(tyokiTexture,tyoki.x , tyoki.y);
+    batch.draw(paTexture, pa.x, pa.y);
+
+    camera.update();
     batch.end();
   }
 }
