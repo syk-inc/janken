5dbc65ea9245ec07be34ab72085afc54c5d6d3d3
sota <git@xsota.com>
2016-06-04 20:15:47 +0900

    ResultScreenにcameraとbatchをおいただけ

--- core/src/inc/syk/janken/screen/ResultScreen.java
+++ core/src/inc/syk/janken/screen/ResultScreen.java
@@ -1,14 +1,29 @@
 package inc.syk.janken.screen;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import inc.syk.janken.JankenGame;
 
 /**
  * Created by sota on 16/06/03.
  */
 public class ResultScreen implements Screen {
+  public static final String LOG_TAG = "ResultScreen";
+
+  private OrthographicCamera camera;
+  private SpriteBatch batch;
+
   @Override
   public void show() {
+    Gdx.app.log(LOG_TAG, "show");
 
+    camera = new OrthographicCamera();
+    camera.setToOrtho(false, JankenGame.SCREEN_SIZE_WIDTH, JankenGame.SCREEN_SIZE_HEIGHT);
+
+    batch = new SpriteBatch();
   }
 
   @Override
