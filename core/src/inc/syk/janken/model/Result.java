b080456d96d2e4ef9dd383738dccefddf4d073e4
sota <git@xsota.com>
2016-06-12 23:58:03 +0900

    なんか必要そうなメソッドを追加してみた

--- core/src/inc/syk/janken/model/Result.java
+++ core/src/inc/syk/janken/model/Result.java
@@ -4,10 +4,32 @@ package inc.syk.janken.model;
  * Created by sota on 16/06/12.
  */
 public class Result {
-  private Long score;
-  private Long winCount;
-  private Long loseCount;
-  private Long drawCount;
+  private Long score = Long.valueOf(0);
+  private Long winCount = Long.valueOf(0);
+  private Long loseCount = Long.valueOf(0);
+  private Long drawCount = Long.valueOf(0);
+
+  /**
+   * じゃんけんに勝った
+   */
+  public void win(){
+    winCount += 1;
+    score += 10;
+  }
+
+  /**
+   * じゃんけんに負けた
+   */
+  public void lose(){
+    loseCount += 1;
+  }
+
+  /**
+   * じゃんけんに引き分けた
+   */
+  public void draw(){
+    drawCount += 1;
+  }
 
   public Long getScore() {
     return score;
