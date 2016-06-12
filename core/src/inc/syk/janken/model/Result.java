8c6d57cde8d21c82396ccd78ba7b13cc86ad7693
sota <git@xsota.com>
2016-06-12 22:09:13 +0900

    ResultModelを仮に作った

---
+++ core/src/inc/syk/janken/model/Result.java
@@ -0,0 +1,27 @@
+package inc.syk.janken.model;
+
+/**
+ * Created by sota on 16/06/12.
+ */
+public class Result {
+  private Long score;
+  private Long winCount;
+  private Long loseCount;
+  private Long drawCount;
+
+  public Long getScore() {
+    return score;
+  }
+
+  public Long getWinCount() {
+    return winCount;
+  }
+
+  public Long getLoseCount() {
+    return loseCount;
+  }
+
+  public Long getDrawCount() {
+    return drawCount;
+  }
+}
