40a15124ccca480b438da816ecb5a065f007de20
yamaxy02 <yamaxy02@gmail.com>
2016-06-17 00:25:56 +0900

    タッチした場所からじゃんけんの判定をするところまで

--- core/src/inc/syk/janken/screen/GameScreen.java
+++ core/src/inc/syk/janken/screen/GameScreen.java
@@ -2,12 +2,16 @@ package inc.syk.janken.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.TimeUtils;
 
 import inc.syk.janken.JankenGame;
 
@@ -20,6 +24,7 @@ public class GameScreen implements Screen {
   private OrthographicCamera camera;
 
   private SpriteBatch batch;
+  private BitmapFont font;
 
   private Texture guTexture;
   private Texture tyokiTexture;
@@ -28,6 +33,10 @@ public class GameScreen implements Screen {
   private Rectangle gu;
   private Rectangle tyoki;
   private Rectangle pa;
+
+  private long enemyhand;
+  private long myhand;
+  private long kekka;
 
   private JankenGame game ;
 
@@ -43,6 +52,8 @@ public class GameScreen implements Screen {
     camera.setToOrtho(false, JankenGame.SCREEN_SIZE_WIDTH, JankenGame.SCREEN_SIZE_HEIGHT); //画面サイズ
 
     batch = new SpriteBatch();
+    font = new BitmapFont();
+    font.setColor(Color.RED);
 
     guTexture = new Texture("guu.png");
     tyokiTexture = new Texture("tyoki.png");
@@ -74,20 +85,105 @@ public class GameScreen implements Screen {
     Gdx.gl.glClearColor(1, 1, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+    /*敵の手をランダムで出す*/
+    enemyhand = TimeUtils.millis() % 3;
+    /*
+    System.out.println(enemyhand);
+    +/
+
+    /*タッチした場所による条件分け*/
+    Vector3 tmp = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+    camera.unproject(tmp);
+
+    if (gu.contains(tmp.x, tmp.y)) {
+      System.out.println("Is touched gu");
+      myhand = 0;
+      touchedgu();
+    }
+
+    if (tyoki.contains(tmp.x, tmp.y)) {
+      System.out.println("Is touched tyoki");
+      myhand = 1;
+      touchedtyoki();
+    }
+
+    if (pa.contains(tmp.x, tmp.y)) {
+      System.out.println("Is touched pa");
+      myhand = 2;
+      touchedpa();
+    }
+
+    /*batchでの画像表示はrenderの最後に行う*/
     batch.setProjectionMatrix(camera.combined);
     batch.begin();
-
 
     batch.draw(guTexture, gu.x, gu.y);
     batch.draw(tyokiTexture,tyoki.x , tyoki.y);
     batch.draw(paTexture, pa.x, pa.y);
 
-    Vector3 tmp = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-    camera.unproject(tmp);
-
+    /*　結果を表示する
+    font.draw(batch, "Hello World", 200, 200);
+    */
     camera.update();
     batch.end();
   }
+
+  /*グーチョキパーの条件付け
+  *グー　0　チョキ　1　パー　2
+  * kekka kati 0 hikiwake 1 make 2
+  */
+  private void touchedgu() {
+    Gdx.gl.glClearColor(1, 0, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    if(enemyhand == 0){
+      kekka = 1;
+      System.out.println("ひきわけ");
+    }
+    if(enemyhand == 1){
+      kekka = 0;
+      System.out.println("かち");
+    }
+    if(enemyhand == 2){
+      kekka = 2;
+      System.out.println("まけ");
+    }
+  }
+
+  private void touchedtyoki() {
+    Gdx.gl.glClearColor(1, 1, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    if(enemyhand == 0){
+      kekka = 2;
+      System.out.println("まけ");
+    }
+    if(enemyhand == 1){
+      kekka = 1;
+      System.out.println("ひきわけ");
+    }
+    if(enemyhand == 2){
+      kekka = 0;
+      System.out.println("かち");
+    }
+  }
+
+  private void touchedpa() {
+    Gdx.gl.glClearColor(0, 1, 1, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    if(enemyhand == 0){
+      kekka = 0;
+      System.out.println("かち");
+    }
+    if(enemyhand == 1){
+      kekka = 2;
+      System.out.println("まけ");
+    }
+    if(enemyhand == 2){
+      kekka = 1;
+      System.out.println("ひきわけ");
+    }
+  }
+
+  /*条件付けここまで*/
 
   @Override
   public void resize(int width, int height) {
